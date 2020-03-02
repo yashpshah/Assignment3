f343040efe2d77ec9f9b909248ba9b3dd11f879a
yashpshah <60555137+yashpshah@users.noreply.github.com>
2020-03-01 22:59:16 -0800

    updated

--- src/app.jsx
+++ src/app.jsx
@@ -128,10 +128,6 @@ class ProductList extends React.Component {
 }
 
 async createProduct(newProduct) {
-    const newProducts = this.state.products.slice();
-    newProduct.id = this.state.products.length + 1;
-    newProducts.push(newProduct);
-    this.setState({ products: newProducts });
     const query = `mutation {
         productAdd(product:{
           Name: "${newProduct.product_name}",
